5d9f5a94409be456f05ae1932cf24b507c0abbb7
Rom <rom.valme@gmail.com>
2019-04-29 22:47:39 -0400

    adding prototype list artists feature

--- twitter.py
+++ twitter.py
@@ -1,6 +1,6 @@
 import numpy as np
 import json
-from tweet import Tweet, NewsFeed
+from tweet import Tweet, NewsFeed, Artist, LocalArtists
 import random
 import time
 
@@ -10,8 +10,20 @@ def setup():
     with open("cs_546.json") as json_file:
         tweet_texts = json.load(json_file)
 
+    with open("artist.json") as json_file:
+        artists = json.load(json_file)
+
     Tweets = {}
+    Artists = {}
     tid = []
+    aid = []
+    for _ in xrange(18):
+        ard = random.choice(artists['artists'])
+        ar = Artist(ard)
+        aid.append(ar.id)
+        Artists[ar.id] = ar
+    LocalA = LocalArtists(Artists)
+
     for _ in xrange(100): #tweet_texts['tweets']:
         tt = random.choice(tweet_texts['tweets'])
         tw = Tweet(tt)
@@ -21,26 +33,22 @@ def setup():
         tid.append(tw.id)
         Tweets[tw.id] = tw
     Newsf = NewsFeed(Tweets)
-    '''
-    import pdb;pdb.set_trace()
-    for tw in Tweets:
-        Tweets[tw].print_tweet()
-    '''
-    return Newsf
-    #Newsf.print_newsfeed()
+    return (Newsf,LocalA)
 #---------------------------------------
 
 
 
 
-def start_loop(Newsf):
+def start_loop(Newsf, LocalArtists):
 #LOOP GUTS pn - Print News Feed, fn - Filter News Feed, - End Session
 #---------------------------------------
     inp = ''
     while(1):
-        print("pn - Print News Feed, fn topic - Filter NewsFeed, e End Session")
+        print("pn - Print News Feed, fn topic - Filter NewsFeed, e End Session, pa - Print Artists")
         inp = raw_input("Enter: ")
         inp = inp.split(' ')
+        if(inp[0] == 'pa'):
+            LocalArtists.print_artists()
         if(inp[0] == 'pn'):
             Newsf.print_newsfeed()
         elif(inp[0] == 'e'):
@@ -50,7 +58,7 @@ def start_loop(Newsf):
             Newsf.print_filtered_nf()
 
 if __name__ == "__main__":
-    Newsf = setup()
-    start_loop(Newsf)
+    Newsf, LocalArtists = setup()
+    start_loop(Newsf, LocalArtists)
 
 
